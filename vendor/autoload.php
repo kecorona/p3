8e8fcd2a9378e187e451a91e7d77962da2d91b28
Kristin Corona <kristincorona@gmail.com>
2014-10-30 21:54:52 -0400

    revert debug

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit0a6f31fae20cff554455146b44eb71f6::getLoader();
+return ComposerAutoloaderIniteb764c44d0ce80c80b95b636e72dc16b::getLoader();
