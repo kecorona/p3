e16e6cbd0748b1690116e4f5cb022bab90efaa8c
Kristin Corona <kristincorona@gmail.com>
2014-10-30 22:21:57 -0400

    clear cache, permissions fix, dump autoload.

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitd5060d1515ed0916e8136fb086cd7004::getLoader();
+return ComposerAutoloaderInit10ae5435e916835385a8ac3828cf18c8::getLoader();
