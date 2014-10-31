b4f44ba989a371499578260a824ca81fd9845718
Kristin Corona <kristincorona@gmail.com>
2014-10-31 12:44:00 -0400

    typo

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once '../../vendor/fzaninotto/faker/src/autoload.php';
+require_once ('/../../vendor/fzaninotto/faker/src/autoload.php');
 
 class UserController extends BaseController {
 	/*
