918a1e6ffff4b2b1a445debfff608507518ac54f
Kristin Corona <kristincorona@gmail.com>
2014-11-07 18:17:05 -0500

    change hostname config

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -27,7 +27,7 @@ $app = new Illuminate\Foundation\Application;
 $env = $app->detectEnvironment(array(
 
 	'local' => array('*'),
-	'production' => array('hemingway'),
+	'production' => array('kcorona'),
 
 ));
 
