141816d98dbf4da55ff4a535bf7219e4fcffa8ad
Kristin Corona <kristincorona@gmail.com>
2014-11-20 16:05:14 -0500

    add error handler

--- app/filters.php
+++ app/filters.php
@@ -56,6 +56,42 @@ Route::filter('auth.basic', function()
 
 /*
 |--------------------------------------------------------------------------
+| Application Error Handler
+|--------------------------------------------------------------------------
+|
+| Here you may handle any errors that occur in your application, including
+| logging them or displaying custom views for specific errors. You may
+| even register several error handlers to handle different types of
+| exceptions. If nothing is returned, the default error view is
+| shown, which includes a detailed stack trace during debug.
+|
+*/
+
+App::error(function(Exception $exception, $code)
+{
+    $pathInfo = Request::getPathInfo();
+    $message = $exception->getMessage() ?: 'Exception';
+    Log::error("$code - $message @ $pathInfo\r\n$exception");
+
+    if (Config::get('app.debug')) {
+        return;
+    }
+
+    switch ($code)
+    {
+        case 403:
+            return Response::view('errors/403', array(), 403);
+
+        case 500:
+            return Response::view('errors/500', array(), 500);
+
+        default:
+            return Response::view('errors/404', array(), $code);
+    }
+});
+
+/*
+|--------------------------------------------------------------------------
 | Guest Filter
 |--------------------------------------------------------------------------
 |
